e615b0b9e88afb943530a698f73642bf220a2d0e
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2023-10-17 15:21:17 +0200

    updat
    update

--- src/assets/login/login.jsx
+++ src/assets/login/login.jsx
@@ -8,13 +8,13 @@ function Login() {
         <div className="login container">
             <h1>LCKD</h1>
             <h3>KEEPING YOUR PASSWORDS SAFE</h3>
-            <div>
+            <div className="login__inputs">
                 <small>USERNAME</small>
                 <input type="text" placeholder="username" />
                 <small>PASSWORD</small>
                 <input type="text" placeholder="password"/>
             </div>
-            <button>LET ME IN</button>
+            <button className="btn">LET ME IN</button>
         
         </div>
     )
